e55e6757d03e72e19d2e93987c49d428e271a20d
Geoffery joseph <geofferyjoseph1@gmail.com>
2020-06-17 18:49:06 +0100

    debug to true

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$%62fj*^&#!)&pnx@ex*o17hb(m$u!&1bbt&mqm9bymrse$v$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
