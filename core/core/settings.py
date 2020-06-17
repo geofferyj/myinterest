da49d6119a577b0ba4c7221f89ca2610b3b9a317
Geoffery joseph <geofferyjoseph1@gmail.com>
2020-06-17 19:02:37 +0100

    collect static

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$%62fj*^&#!)&pnx@ex*o17hb(m$u!&1bbt&mqm9bymrse$v$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["*"]
